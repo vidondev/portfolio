e0cfd58f9af3fc7a6c10f905bc94d0cd20eb42ef
Vidon <vidon.dev@gmail.com>
2021-06-21 23:14:25 +0800

    Add files via upload

---
+++ docs/music-app/precache-manifest.41591a39a000259408017ef6f3552f5f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47addccd25e79aae857973084ad44e4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "948a717b45a331a47859",
+    "url": "/static/js/2.9ea1b4fa.chunk.js"
+  },
+  {
+    "revision": "036c0c93fc327f7a170d",
+    "url": "/static/js/main.057826af.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
